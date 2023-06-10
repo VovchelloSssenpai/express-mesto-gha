ea85c1ee2b9a71132f1778b86e05d27f94513eff
acer <vova.mkhitaryan94@gmail.com>
2023-06-10 21:15:54 +0400

    11 test

--- controllers/cards.js
+++ controllers/cards.js
@@ -1,85 +1,85 @@
 const Card = require('../models/card');
+const {
+  INCORRECT_ERROR_CODE,
+  NOT_FOUND_ERROR_CODE,
+  DEFAULT_ERROR_CODE,
+} = require('../utils/utils');
 
-const getCards = ('/cards', (req, res) => {
+const getCards = ((req, res) => {
   Card.find({}).then((cardsData) => res.send(cardsData))
-    .catch((err) => {
-      res.status(500).send({
+    .catch(() => {
+      res.status(DEFAULT_ERROR_CODE).send({
         message: 'Internal Server Error',
-        err: err.message,
-        stack: err.stack,
       });
     });
 });
 
-const deleteCardById = ('/cards/:id', (req, res) => {
+const deleteCardById = ((req, res) => {
   Card.findByIdAndRemove(req.params.id)
     .orFail(() => new Error('Not found'))
-    .then((user) => { res.status(200).send(user); })
+    .then((user) => { if (!user) { return res.status(NOT_FOUND_ERROR_CODE).send({ message: 'Запрашиваемый пользователь не найден' }); } return res.send(user); })
     .catch((err) => {
       if (err.message === 'Not found') {
-        return res.status(404).send({ message: 'Вы ввели некоректный ID' });
+        return res.status(NOT_FOUND_ERROR_CODE).send({ message: 'Вы ввели некоректный ID' });
       }
       if (err.name.includes('CastError')) {
-        return res.status(400).send({ message: 'Вы ввели некоректные данные' });
+        return res.status(INCORRECT_ERROR_CODE).send({ message: 'Вы ввели некоректные данные' });
       }
-      return res.status(500).send({
+      return res.status(DEFAULT_ERROR_CODE).send({
         message: 'Internal Server Error',
-        err: err.message,
-        stack: err.stack,
       });
     });
 });
 
-const createCard = ('/cards', (req, res) => {
-  Card.create({ ...req.body, owner: req.user._id })
+const createCard = ((req, res) => {
+  const cardData = {
+    name: req.body.name,
+    link: req.body.link,
+    owner: req.user._id,
+  };
+
+  Card.create(cardData)
     .then((user) => res.status(201).send(user))
     .catch((err) => {
       if (err.message.includes('validation failed')) {
-        return res.status(400).send({ message: 'Вы ввели некоректные данные' });
+        return res.status(INCORRECT_ERROR_CODE).send({ message: 'Вы ввели некоректные данные' });
       }
 
-      return res.status(500).send({
+      return res.status(DEFAULT_ERROR_CODE).send({
         message: 'Internal Server Error',
-        err: err.message,
-        stack: err.stack,
       });
     });
 });
 
-const likeCard = ('/cards', (req, res) => {
+const likeCard = ((req, res) => {
   Card.findByIdAndUpdate(
     req.params.cardId,
     { $addToSet: { likes: req.user._id } },
     { new: true },
   )
     .orFail(() => new Error('Not found'))
-    .then((user) => res.status(201).send(user))
+    .then((user) => { if (!user) { return res.status(NOT_FOUND_ERROR_CODE).send({ message: 'Запрашиваемый пользователь не найден' }); } return res.status(201).send(user); })
     .catch((err) => {
-      if (err.stack.includes('Not found')) {
-        return res.status(404).send({ message: 'Вы ввели некоректный ID' });
-      }
       if (err.name.includes('CastError')) {
-        return res.status(400).send({ message: 'Вы ввели некоректные данные' });
+        return res.status(INCORRECT_ERROR_CODE).send({ message: 'Вы ввели некоректные данные' });
       }
-      return res.status(500).send({
+      return res.status(DEFAULT_ERROR_CODE).send({
         message: 'Internal Server Error',
-        err: err.message,
-        stack: err.stack,
       });
     });
 }
 );
 
-const dislikeCard = ('/cards', (req, res) => Card.findByIdAndUpdate(
+const dislikeCard = ((req, res) => Card.findByIdAndUpdate(
   req.params.cardId,
   { $pull: { likes: req.user._id } },
   { new: true },
-).then((user) => { if (user === null) { return res.status(404).send({ message: 'Вы ввели некоректный ID' }); } return res.status(200).send(user); })
+).then((user) => { if (user === null) { return res.status(NOT_FOUND_ERROR_CODE).send({ message: 'Вы ввели некоректный ID' }); } return res.status(200).send(user); })
   .catch((err) => {
     if (err.name.includes('CastError')) {
-      return res.status(400).send({ message: 'Вы ввели некоректные данные' });
+      return res.status(INCORRECT_ERROR_CODE).send({ message: 'Вы ввели некоректные данные' });
     }
-    return res.status(500).send({
+    return res.status(DEFAULT_ERROR_CODE).send({
       message: 'Internal Server Error',
       err: err.message,
       stack: err.stack,
--- controllers/users.js
+++ controllers/users.js
@@ -1,95 +1,89 @@
 const User = require('../models/user');
+const {
+  INCORRECT_ERROR_CODE,
+  NOT_FOUND_ERROR_CODE,
+  DEFAULT_ERROR_CODE,
+} = require('../utils/utils');
 
-const getUserById = ('/users/:id',
-(req, res) => {
-  User.findById(req.params.id)
-    .orFail(() => new Error('Not found'))
-    .then((user) => { res.status(200).send(user); })
-    .catch((err) => {
-      if (req.params.id.length === 24) {
-        return res.status(404).send({ message: 'Вы ввели некоректный ID' });
-      }
-      if (req.params.id.length !== '24') {
-        return res.status(400).send({ message: 'Вы ввели некоректные данные' });
-      }
-      return res.status(500).send({
-        message: 'Internal Server Error',
-        err: err.message,
-        stack: err.stack,
+const getUserById = (
+  (req, res) => {
+    User.findById(req.params.id)
+      .orFail(() => new Error('Not found'))
+      .then((user) => { res.send(user); })
+      .catch((err) => {
+        if (err.message === 'Not found') {
+          return res.status(NOT_FOUND_ERROR_CODE).send({ message: 'Вы ввели некоректный ID' });
+        }
+        if (err.name === 'CastError') {
+          return res.status(INCORRECT_ERROR_CODE).send({ message: 'Вы ввели некоректные данные' });
+        }
+        return res.status(DEFAULT_ERROR_CODE).send({
+          message: 'Internal Server Error',
+        });
       });
-    });
-});
+  });
 
-const getUsers = ('/users',
-(req, res) => {
-  User.find({})
-    .then((usersData) => res.send(usersData))
-    .catch((err) => {
-      res.status(500).send({
-        message: 'Internal Server Error',
-        err: err.message,
-        stack: err.stack,
+const getUsers = (
+  (req, res) => {
+    User.find({})
+      .then((usersData) => res.send(usersData))
+      .catch(() => {
+        res.status(DEFAULT_ERROR_CODE).send({
+          message: 'Internal Server Error',
+        });
       });
-    });
-});
+  });
 
-const createUser = ('/users',
-(req, res) => {
-  User.create(req.body)
-    .then((user) => res.status(201).send(user))
-    .catch((err) => {
-      if (err.message.includes('validation failed')) {
-        return res.status(400).send({ message: 'Вы ввели некоректные данные' });
-      }
+const createUser = (
+  (req, res) => {
+    const userData = {
+      name: req.body.name,
+      about: req.body.about,
+      avatar: req.body.avatar,
+      owner: req.user._id,
+    };
 
-      return res.status(500).send({
-        message: 'Internal Server Error',
-        err: err.message,
-        stack: err.stack,
+    User.create(userData)
+      .then((user) => res.status(201).send(user))
+      .catch((err) => {
+        if (err.message.includes('validation failed')) {
+          return res.status(INCORRECT_ERROR_CODE).send({ message: 'Вы ввели некоректные данные' });
+        }
+        return res.status(DEFAULT_ERROR_CODE).send({
+          message: 'Internal Server Error',
+        });
       });
-    });
-});
+  });
 
-const updateUser = ('/users',
-(req, res) => {
-  const { name, about } = req.body;
-  User.findByIdAndUpdate(req.user._id, { name, about }, { new: true, runValidators: true })
-    .then((user) => res.status(200).send(user))
-    .catch((err) => {
-      if (err.message.includes('Validation failed')) {
-        return res.status(400).send({ message: 'Вы ввели некоректные данные' });
-      }
-      if (err.message.includes('ObjectId failed')) {
-        return res.status(404).send({ message: 'Запрашиваемый пользователь не найден' });
-      }
-      return res.status(500).send({
-        message: 'Internal Server Error',
-        err: err.message,
-        stack: err.stack,
+const updateUser = (
+  (req, res) => {
+    const { name, about } = req.body;
+    User.findByIdAndUpdate(req.user._id, { name, about }, { new: true, runValidators: true })
+      .then((user) => { if (!user) { return res.status(NOT_FOUND_ERROR_CODE).send({ message: 'Запрашиваемый пользователь не найден' }); } return res.send(user); })
+      .catch((err) => {
+        if (err.message.includes('Validation failed')) {
+          return res.status(INCORRECT_ERROR_CODE).send({ message: 'Вы ввели некоректные данные' });
+        }
+        return res.status(DEFAULT_ERROR_CODE).send({
+          message: 'Internal Server Error',
+        });
       });
-    });
-});
+  });
 
-const updateAvatar = ('/users',
-(req, res) => {
-  const { avatar } = req.body;
-  User.findByIdAndUpdate(req.user._id, { avatar }, { new: true, runValidators: true })
-    .then((user) => res.status(200).send(user))
-    .catch((err) => {
-      if (err.message.includes('Validation failed')) {
-        return res.status(400).send({ message: 'Вы ввели некоректные данные' });
-      }
-      if (err.message.includes('ObjectId failed')) {
-        return res.status(404).send({ message: 'Запрашиваемый пользователь не найден' });
-      }
-
-      return res.status(500).send({
-        message: 'Internal Server Error',
-        err: err.message,
-        stack: err.stack,
+const updateAvatar = (
+  (req, res) => {
+    const { avatar } = req.body;
+    User.findByIdAndUpdate(req.user._id, { avatar }, { new: true, runValidators: true })
+      .then((user) => { if (!user) { return res.status(NOT_FOUND_ERROR_CODE).send({ message: 'Запрашиваемый пользователь не найден' }); } return res.send(user); })
+      .catch((err) => {
+        if (err.message.includes('Validation failed')) {
+          return res.status(INCORRECT_ERROR_CODE).send({ message: 'Вы ввели некоректные данные' });
+        }
+        return res.status(DEFAULT_ERROR_CODE).send({
+          message: 'Internal Server Error',
+        });
       });
-    });
-});
+  });
 
 module.exports = {
   getUserById,
