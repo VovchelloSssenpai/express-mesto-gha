a29a92b3ab6f01c834d5a638ec2a2d838654ad5b
acer <vova.mkhitaryan94@gmail.com>
2023-06-22 16:57:43 +0400

    17 test

--- routes/cards.js
+++ routes/cards.js
@@ -1,12 +1,30 @@
 const router = require('express').Router();
+const { celebrate, Joi } = require('celebrate');
 const {
   getCards, deleteCardById, createCard, likeCard, dislikeCard,
 } = require('../controllers/cards');
 
 router.get('/', getCards);
-router.delete('/:id', deleteCardById);
-router.post('/', createCard);
-router.put('/:cardId/likes', likeCard);
-router.delete('/:cardId/likes', dislikeCard);
+router.delete('/:id', celebrate({
+  params: Joi.object().keys({
+    id: Joi.string().alphanum().length(24),
+  }),
+}), deleteCardById);
+router.post('/', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().min(2).max(30).required(),
+    link: Joi.string().uri().required(),
+  }),
+}), createCard);
+router.put('/:cardId/likes', celebrate({
+  params: Joi.object().keys({
+    cardId: Joi.string().alphanum().length(24),
+  }),
+}), likeCard);
+router.delete('/:cardId/likes', celebrate({
+  params: Joi.object().keys({
+    cardId: Joi.string().alphanum().length(24),
+  }),
+}), dislikeCard);
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,15 +1,26 @@
 const router = require('express').Router();
+const { celebrate, Joi } = require('celebrate');
 const {
   getUserById, getUsers, updateUser, updateAvatar, getUser,
 } = require('../controllers/users');
 
 router.get('/me', getUser);
 
-router.get('/:id', getUserById);
+router.get('/:id', celebrate({
+  params: Joi.object().keys({
+    id: Joi.string().alphanum().length(24),
+  }),
+}), getUserById);
 
 router.get('/', getUsers);
 
-router.patch('/me', updateUser);
+router.patch('/me', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().min(2).max(30).default('Жак-Ив Кусто'),
+    about: Joi.string().min(2).max(30).default('Исследователь'),
+    avatar: Joi.string().default('https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png'),
+  }),
+}), updateUser);
 
 router.patch('/me/avatar', updateAvatar);
 
